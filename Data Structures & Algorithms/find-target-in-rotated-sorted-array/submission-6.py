66f42cfd18d8769cbd7e903ff2e9be527a52e19a
Sriramm S S <108517615+SriRammSS@users.noreply.github.com>
2026-03-31 23:21:03 -0400

    Add: find-target-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low=0
+        high=len(nums)-1
+# We will try to see which half of the side is sorted and see if the element exisit in that range or not.We search sorted because once we know it is sorted we can run binary search on that and see if the elemnt is presnt or not
+#. If it doesnt exisit in that part or half we explore the other half.
+        while low<=high: #Here we use low<=high because the element might or might not exisit
+            mid=low+((high-low)//2)
+
+            if nums[mid]==target:
+                return mid
+
+            if nums[low]<=nums[mid]: #Searching the left half from mid to see if it is sorted 
+                if nums[low]<=target<nums[mid]:
+                    high=mid-1
+                else:
+                    low=mid+1
+            else:
+                if nums[mid]<=target<=nums[high]: #This means the left half is not sorted.
+                    low=mid+1
+                else:
+                    high=mid-1
+        return low if low<=high else -1
+
+                    
+                
+
+

c57ecca9fd25d306499ee8a49eaa5f84d5e170ed
Sriramm S S <108517615+SriRammSS@users.noreply.github.com>
2026-04-03 01:03:21 -0400

    Add: linked-list-cycle-detection - submission-5

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-5.py
@@ -0,0 +1,23 @@
+class ListNode:
+   def __init__(self, val=0, next=None):
+       self.val = val
+       self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        if head is None:
+            return False
+        pointer1=head
+        pointer2=head
+        
+
+        while pointer1 and pointer2 :
+            pointer1=pointer1.next
+            pointer2=pointer2.next.next
+
+            if pointer1==pointer2:
+                return True
+        return False
+
+        

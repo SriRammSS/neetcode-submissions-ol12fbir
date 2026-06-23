e99d0b10476e3be24b2d82e26a94bca1780cd227
Sriramm S S <108517615+SriRammSS@users.noreply.github.com>
2026-06-22 22:18:26 -0500

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        track={}
+
+        max_length=0
+
+        j=0
+
+        for i in range(len(s)):
+
+            if s[i] in track.keys():
+                track[s[i]]=track[s[i]]+1
+            else:
+                track[s[i]]=1
+
+            if i-j-max(track.values())+1>k:
+                track[s[j]]=track[s[j]]-1
+                j=j+1
+            
+            max_length=max(max_length,i-j+1)
+        
+        return max_length
+
+            
+
+            
+
+
+
+
+        

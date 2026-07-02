c5c25d68e9e722999b0f60060038722736130ffc
Sriramm S S <108517615+SriRammSS@users.noreply.github.com>
2026-07-01 23:03:47 -0500

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        fleet=set()
+
+        for i in range(len(position)):
+            need=(target-position[i])/speed[i]
+
+          
+            fleet.add(int(need))
+
+            
+
+            
+        
+        return len(fleet)
+        

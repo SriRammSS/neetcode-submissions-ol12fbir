81aff580656a3ff20f446c60652579024ad523c0
Sriramm S S <108517615+SriRammSS@users.noreply.github.com>
2026-07-01 22:56:52 -0500

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        track=set()
+
+        for i in range(len(position)):
+            need=(target-position[i])//speed[i]
+
+            track.add(need)
+        
+        return len(track)
+        

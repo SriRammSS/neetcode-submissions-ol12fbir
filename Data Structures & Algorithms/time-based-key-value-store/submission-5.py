acc36190a5a0bfb76f58cc0183145491704450a7
Sriramm S S <108517615+SriRammSS@users.noreply.github.com>
2026-07-05 18:14:32 -0500

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,25 @@
+class TimeMap:
+
+    def __init__(self):
+        self.timemap={}
+        self.timestamp_record=[]
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.timestamp_record.append(timestamp)
+        keys=(key,timestamp)
+        self.timemap[keys]=value
+
+    def get(self, key: str, timestamp: int) -> str:
+        keys=(key,timestamp)
+        if keys in self.timemap.keys():
+            return self.timemap.get(keys)
+        else:
+            for i in range(len(self.timestamp_record)):
+                if self.timestamp_record[i]<timestamp:
+                    low=self.timestamp_record[i]
+            
+            if self.timestamp_record[-1]<=timestamp:
+                return self.timemap.get((key,low))
+            else:
+                return ""
+            
+        
